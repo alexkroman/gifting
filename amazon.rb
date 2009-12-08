02adaf0b4e5fa99eec363ef2cb9e075097b6b540
Alex Kroman <alex@thomas-kings-macbook-pro-2.local>
2009-12-07 22:19:54 -0800

    test

--- amazon.rb
+++ amazon.rb
@@ -3,8 +3,26 @@ require 'sinatra'
 require 'erb'
 require 'amazon/ecs'
 require 'model'
+require 'facebooker'
+require 'haml'
 
 enable :sessions
+raise "No api_keys.yml was found" unless File.exist?(api_keys_file = "api_keys.yml")
+$keys = YAML.load(File.read(api_keys_file))
+$api_key, $secret_key = $keys['api_key'], $keys['secret_key']
+
+helpers do
+  def logged_in?
+    facebook_user
+  end
+  def facebook_user
+    @facebook_user ||= if request.cookies[$api_key]
+      fb_session = Facebooker::Session.new($api_key, $secret_key)
+      fb_session.secure_with! *%w(session_key user expires ss).map {|k| request.cookies[$api_key + "_" + k]}
+      fb_session.user
+    end
+  end
+end
 
 def cache_control
   headers['Cache-Control'] = 'max-age=900, public'
@@ -65,6 +83,14 @@ get '/vote' do
     create_survey(true)
   end
   redirect '/search?tags=' + params[:tags] 
+end
+
+get '/login' do
+  haml :login
+end
+
+get '/connect' do
+  haml :connect
 end
 
 def create_survey(like)
