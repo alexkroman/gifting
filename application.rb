d82864bce9f17bf16e5d666389e65312978d7982
Alex Kroman <alex@Alex-Kromans-MacBook-Pro.local>
2011-05-30 12:09:25 -0700

    test

--- application.rb
+++ application.rb
@@ -7,6 +7,6 @@ require 'cgi'
 require 'stringex'
 
 get '/' do
-  @products = Product.order(:sales_rank).all.group_by(&:category).sort_by{rand(srand)}[0..39]
+  @products = Product.order(:sales_rank).all.group_by(&:category).sort_by{rand(srand)}[0..19]
   erb :index
 end
